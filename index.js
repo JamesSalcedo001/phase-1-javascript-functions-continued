5011d508a5ce0906727beb9e395ee967ed3d43e2
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-08-15 22:28:38 -0500

    commit tree 6868b0fcf73ff88d67fac493cb6ada6ecf528595

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+
+
+  function wrapAdjective(v1 = '*', v2 = '||') {
+    return function (v3 = "special") {
+        return `You are ${v1}${v3}${v1}!` ;
+    };
+  }
+  
